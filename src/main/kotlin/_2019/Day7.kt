cdf14d476e33002ac8baf003f27b55285cdbb271
bright_spark <markpassey@btinternet.com>
2019-12-16 10:04:34 +0000

    Fixed mistake

--- src/main/kotlin/_2019/Day7.kt
+++ src/main/kotlin/_2019/Day7.kt
@@ -23,7 +23,7 @@ fun main() {
         }
     }
 
-    aocRun(testInput1) { input ->
+    aocRun(puzzleInput) { input ->
         val code = IntcodeComputer.parseCode(input)
         val computer = IntcodeComputer()
         var max = Int.MIN_VALUE
@@ -31,16 +31,16 @@ fun main() {
             val ampA = runAmplifier(computer, code, "A", a)
             phaseSettingValues.forEach b@{ b ->
                 if (b == a) return@b
-                val ampB = runAmplifier(computer, code, "B", a, ampA)
+                val ampB = runAmplifier(computer, code, "B", b, ampA)
                 phaseSettingValues.forEach c@{ c ->
                     if (c == a || c == b) return@c
-                    val ampC = runAmplifier(computer, code, "C", a, ampB)
+                    val ampC = runAmplifier(computer, code, "C", c, ampB)
                     phaseSettingValues.forEach d@{ d ->
                         if (d == a || d == b || d == c) return@d
-                        val ampD = runAmplifier(computer, code, "D", a, ampC)
+                        val ampD = runAmplifier(computer, code, "D", d, ampC)
                         phaseSettingValues.forEach e@{ e ->
                             if (e == a || e == b || e == c || e == d) return@e
-                            val ampE = runAmplifier(computer, code, "E", a, ampD)
+                            val ampE = runAmplifier(computer, code, "E", e, ampD)
                             println("$a$b$c$d$e -> $ampE")
                             if (ampE > max)
                                 max = ampE
@@ -59,7 +59,5 @@ private fun runAmplifier(computer: IntcodeComputer, code: List<Int>, amp: String
     println("Executing amp $amp with inputs: $phaseSetting and $inputSignal")
     return computer.init(code.toMutableList(), phaseSetting, inputSignal).execute().lastOutput.toInt()
 }
-
-private const val testInput1 = "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0"
 
 private const val puzzleInput = "3,8,1001,8,10,8,105,1,0,0,21,30,47,60,81,102,183,264,345,426,99999,3,9,1002,9,5,9,4,9,99,3,9,1002,9,5,9,1001,9,4,9,1002,9,4,9,4,9,99,3,9,101,2,9,9,1002,9,4,9,4,9,99,3,9,1001,9,3,9,1002,9,2,9,101,5,9,9,1002,9,2,9,4,9,99,3,9,102,4,9,9,101,4,9,9,1002,9,3,9,101,2,9,9,4,9,99,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,99,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,99,3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,2,9,4,9,99,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,99"
