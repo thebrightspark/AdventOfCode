911a2d7e53cb9900dba96b58b34f9cc6c87cd26f
bright_spark <markpassey@btinternet.com>
2019-12-18 12:10:59 +0000

    Finished Day 10 part 1

--- src/main/kotlin/_2019/Day10.kt
+++ src/main/kotlin/_2019/Day10.kt
@@ -2,19 +2,13 @@ package _2019
 
 import aocRun
 import between
-import kotlin.math.round
+import kotlin.math.abs
 
 fun main() {
-    aocRun(testInput1) { calcBestAsteroid(it) }
-    aocRun(testInput2) { calcBestAsteroid(it) }
-    aocRun(testInput3) { calcBestAsteroid(it) }
-    aocRun(testInput4) { calcBestAsteroid(it) }
-    aocRun(testInput5) { calcBestAsteroid(it) }
     aocRun(puzzleInput) { calcBestAsteroid(it) }
 }
 
 private fun calcBestAsteroid(input: String): String {
-    println(input)
     val asteroidGrid = input.split("\n").map { line -> line.toCharArray().map { it == '#' } }
     val asteroids = mutableMapOf<Pair<Int, Int>, Int>()
     asteroidGrid.forEachIndexed y1@{ y1, line1 ->
@@ -24,24 +18,7 @@ private fun calcBestAsteroid(input: String): String {
             asteroidGrid.forEachIndexed y2@{ y2, line2 ->
                 line2.forEachIndexed x2@{ x2, isAsteroid2 ->
                     if (!isAsteroid2 || (x1 == x2 && y1 == y2)) return@x2
-                    val canSee = when {
-                        x1 == x2 -> between(y1, y2).map { x1 to it }.none { asteroidGrid[it.second][it.first] }
-                        y1 == y2 -> between(x1, x2).map { it to y1 }.none { asteroidGrid[it.second][it.first] }
-                        else -> {
-                            val gradient = (y1.toFloat() - y2.toFloat()) / (x1.toFloat() - x2.toFloat())
-                            val offset = y1.toFloat() - (gradient * x1.toFloat())
-                            between(x1, x2)
-                                .mapNotNull {
-                                    val y = (gradient * it.toFloat()) + offset
-                                    if (round(y) == y)
-                                        it to y.toInt()
-                                    else
-                                        null
-                                }
-                                .none { asteroidGrid[it.second][it.first] }
-                        }
-                    }
-                    if (canSee)
+                    if (canSee(x1, y1, x2, y2, asteroidGrid))
                         numCanSee++
                 }
             }
@@ -55,54 +32,33 @@ private fun calcBestAsteroid(input: String): String {
     return asteroids.maxBy { it.value }!!.toString()
 }
 
+private fun canSee(x1: Int, y1: Int, x2: Int, y2: Int, asteroidGrid: List<List<Boolean>>): Boolean = when {
+    x1 == x2 -> between(y1, y2).map { x1 to it }.none { asteroidGrid[it.second][it.first] }
+    y1 == y2 -> between(x1, x2).map { it to y1 }.none { asteroidGrid[it.second][it.first] }
+    else -> {
+        val xDiff = x2 - x1
+        val yDiff = y2 - y1
+        val gcd = gcd(abs(xDiff), abs(yDiff))
+        val xVec = xDiff / gcd
+        val yVec = yDiff / gcd
+        var xPos = x1 + xVec
+        var yPos = y1 + yVec
+        var r = true
+        while (xPos in between(x1, x2) && yPos in between(y1, y2)) {
+            if (asteroidGrid[yPos][xPos]) {
+                r = false
+                break
+            }
+            xPos += xVec
+            yPos += yVec
+        }
+        r
+    }
+}
+
+private tailrec fun gcd(int1: Int, int2: Int): Int = if (int2 == 0) int1 else gcd(int2, int1 % int2)
+
 private val testInput1 = """
-.#..#
-.....
-#####
-....#
-...##
-""".trimIndent()
-
-private val testInput2 = """
-......#.#.
-#..#.#....
-..#######.
-.#.#.###..
-.#..#.....
-..#....#.#
-#..#....#.
-.##.#..###
-##...#..#.
-.#....####
-""".trimIndent()
-
-private val testInput3 = """
-#.#...#.#.
-.###....#.
-.#....#...
-##.#.#.#.#
-....#.#.#.
-.##..###.#
-..#...##..
-..##....##
-......#...
-.####.###.
-""".trimIndent()
-
-private val testInput4 = """
-.#..#..###
-####.###.#
-....###.#.
-..###.##.#
-##.##.#.#.
-....###..#
-..#.#..#.#
-#..#.#.###
-.##...##.#
-.....#.#..
-""".trimIndent()
-
-private val testInput5 = """
 .#..##.###...#######
 ##.############..##.
 .#.######.########.#
