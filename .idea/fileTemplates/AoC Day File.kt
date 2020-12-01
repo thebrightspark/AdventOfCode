ea958edce2ffd3220fe2f8fdeabaaed36d45bfdc
bright_spark <markpassey@btinternet.com>
2020-12-01 15:23:09 +0000

    Added day file template

---
+++ .idea/fileTemplates/AoC Day File.kt
@@ -0,0 +1,15 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME}
+#end
+#parse("File Header.java")
+
+import aocRun
+
+fun main() {
+    aocRun(puzzleInput) { input ->
+    
+    }
+}
+
+private val puzzleInput = """
+
+""".trimIndent()
