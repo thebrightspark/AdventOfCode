74472a15045763dabf9385ddf15693fdad1843e9
bright_spark <markpassey@btinternet.com>
2022-12-07 00:26:17 +0000

    Day 3 Part 2

--- src/main/kotlin/_2022/Day3.kt
+++ src/main/kotlin/_2022/Day3.kt
@@ -9,35 +9,32 @@ const val UPPER_CODE_OFFSET = 'A'.code - 27
 fun main() {
 	aoc(2022, 3) {
 		aocRun { input ->
-			process(input.split(REGEX_LINE_SEPARATOR))
+			input.split(REGEX_LINE_SEPARATOR).sumOf { contents ->
+				findCommonItemsInRucksack(contents).sumOf { itemPriority(it) }
+			}
 		}
-//        aocRun(testInput) { input ->
-//        }
+		aocRun { input ->
+			input.split(REGEX_LINE_SEPARATOR).chunked(3).sumOf { group ->
+				itemPriority(findCommonItemInGroup(group))
+			}
+		}
 	}
 }
 
-private fun process(input: List<String>): Int = input.sumOf { contents ->
-	findCommonItems(contents).sumOf { itemPriority(it) }
-}
-
-private fun findCommonItems(contents: String): Set<Char> {
+private fun findCommonItemsInRucksack(contents: String): Set<Char> {
 	val indexMid = contents.length / 2
 	val first = contents.substring(0, indexMid).toSet()
 	val second = contents.substring(indexMid).toSet()
-//	println("$contents\n\t$first\n\t$second")
-	val common = first.intersect(second)
-//	println("Common: $common")
-	return common
+	return first.intersect(second)
+}
+
+private fun findCommonItemInGroup(group: List<String>): Char {
+	var commonItems = group[0].toSet()
+	group.subList(1, group.size).forEach {
+		commonItems = commonItems.intersect(it.toSet())
+	}
+	return commonItems.single()
 }
 
 private fun itemPriority(item: Char): Int =
 	if (item in ('a'..'z')) item.code - LOWER_CODE_OFFSET else item.code - UPPER_CODE_OFFSET
-
-private val testInput = """
-vJrwpWtwJgWrhcsFMMfFFhFp
-jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
-PmmdzqPrVvPwwTWBwg
-wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
-ttgJtRGJQctTZtZT
-CrZsJsPPZsGzwwsLwLmpwMDw
-""".trimIndent()
